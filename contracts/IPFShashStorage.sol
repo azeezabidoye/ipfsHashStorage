ca3e1ad61c5b8559fe84e426a5c26de1e608e033
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-15 09:47:44 +0100

    Initial Commit

---
+++ contracts/IPFShashStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract IPFShashStorage {
+    struct File {
+        string fileName;
+        string ipfsHash;
+    }
+
+    mapping(string => File) private files;
+
+    function upload(string memory fileName, string memory ipfsHash) public {
+        require(
+            bytes(files[fileName].ipfsHash).length == 0,
+            "File already exsits"
+        );
+        files[fileName] = File(fileName, ipfsHash);
+    }
+
+    function getIPFSHash(
+        string memory fileName
+    ) public view returns (string memory) {
+        require(bytes(files[fileName].ipfsHash).length > 0, "File not found");
+        return files[fileName].ipfsHash;
+    }
+
+    function isFileStored(string memory fileName) public view returns (bool) {
+        return bytes(files[fileName].ipfsHash).length > 0;
+    }
+}
