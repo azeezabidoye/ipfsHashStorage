14887d747d238702d6f794f38bca09b22d919891
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-18 16:50:54 +0100

    Backend updated

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+app.get("/index.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.post("uploadData", async (req, res) => {
+  let name = req.body.filename;
+  let sampleFile = req.files.file1;
+  let filename = req.files.file1.name;
+
+  async function moveFileToServer() {
+    sampleFile.mv(__dirname, +`${filename}`, (error) => {
+      if (error) {
+        res.status(500).send(error);
+      }
+    });
+    console.log("File uploaded to server succesfully!");
+  }
+});
